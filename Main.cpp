e3ee79867642d875e586d7faf59ffd1450eb4a6b
Kunal Kumar <kmrknl2604@gmail.com>
2024-03-12 02:28:45 +0530

    Initial commit

---
+++ Main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+int main()
+{
+	std::cout << "Hello World!!" << std::endl;
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "FCC OpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create window!!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
